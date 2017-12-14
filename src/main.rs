fbd88f32ad226b2ee04750b24b11139838b1997b
Sebastian Thiel <byronimo@gmail.com>
2017-12-14 14:20:25 +0100

    Basic command-line parsing and the first part of the journey test.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
-#[macro_use] extern crate clap;
+#[macro_use]
+extern crate clap;
 extern crate structopt;
-#[macro_use] extern crate structopt_derive;
+#[macro_use]
+extern crate structopt_derive;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "s3", about = "The share-secrets-safely command-line interface.")]
+struct Opts {
+    #[structopt(short = "v", long = "vault",
+                help = "The configuration file describing the vault.")]
+    vault: String,
+}
 
 fn main() {
+    let app: clap::App = Opts::clap().version(crate_version!());
+    let opts = Opts::from_clap(app.get_matches());
 
+    println!("Parsed opts");
+    println!("{:?}", opts)
 }
--- tests/journey-test.sh
+++ tests/journey-test.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+set -u
 exe=${1:?First argument is the executable under test}
-echo TBD journey test $exe
-exit 42
+
+function title () {
+  echo "-----------------------------------------------------"
+  echo "$@"
+  echo "-----------------------------------------------------"
+}
+
+SUCCESSFULLY=0
+IT_COUNT=0
+function it () {
+  IT_COUNT=$(( IT_COUNT + 1 ))
+  echo 1>&2 -n "$(tput setaf 2)" "$@"
+}
+
+function fail () {
+  echo 1>&2 "$(tput setaf 1)" " - FAIL"
+  exit $IT_COUNT
+}
+
+function run () {
+  local expected_exit_code=$1
+  shift
+  local output=
+  output="$("$@" 2>&1)"
+  
+  if [[ $? == "$expected_exit_code" ]]; then
+    echo 1>&2 "$(tput setaf 2)" " - OK"
+  else
+    echo 1>&2 "$(tput setaf 1)" " - FAIL"
+    echo 1>&2 "$output"
+    exit $IT_COUNT
+  fi
+}
+
+title "'vault' subcommand"
+
+it "requires the vault configuration to be set" && \
+  run $SUCCESSFULLY $exe vault
+
+
+
