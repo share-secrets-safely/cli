64d159d6c4cbf725f49595c11eaefdebe1e3dae0
Sebastian Thiel <byronimo@gmail.com>
2017-12-14 17:41:08 +0100

    First basic usage of the failure crate

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 #[macro_use]
 extern crate clap;
+extern crate failure;
 
 use clap::{App, Arg, ArgMatches};
+use failure::{err_msg, Error, ResultExt};
 
 #[derive(Debug)]
 struct VaultContext {
     vault_path: String,
 }
 
-fn vault_context_from(args: &ArgMatches) -> Result<VaultContext, String> {
+fn vault_context_from(args: &ArgMatches) -> Result<VaultContext, Error> {
     Ok(VaultContext {
         vault_path: args.value_of("config-file")
             .map(ToOwned::to_owned)
-            .ok_or(String::from("expected arg not present"))?,
+            .ok_or(err_msg("expected clap argument was unset"))?,
     })
 }
 
@@ -30,7 +32,9 @@ fn main() {
     let matches: ArgMatches = app.get_matches();
     match matches.subcommand() {
         ("vault", Some(args)) => {
-            let context = vault_context_from(args).unwrap();
+            let context = vault_context_from(args)
+                .context("context creation failed")
+                .unwrap();
             println!("Parsed opts");
             println!("{:?}", context);
         }
