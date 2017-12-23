622870a90e4bd3bdd2753655c040c07e502e079e
Sebastian Thiel <byronimo@gmail.com>
2017-12-23 14:09:28 +0100

    Put 'init' into own function.

--- lib/vault/src/lib.rs
+++ lib/vault/src/lib.rs
@@ -50,56 +50,54 @@ impl Vault {
     }
 
     fn to_file(&self, path: &str) -> Result<(), VaultError> {
-        OpenOptions::new()
+        let mut file = OpenOptions::new()
             .create(true)
             .write(true)
             .truncate(true)
             .open(path)
-            .map_err(|cause| VaultError::from_io_err(cause, path, IOMode::Write))
-            .and_then(|mut w| {
-                serde_yaml::to_writer(&w, self)
-                    .map_err(|cause| VaultError::Serialization {
-                        cause,
-                        path: path.to_owned(),
-                    })
-                    .and_then(|_| {
-                        writeln!(w)
-                            .map_err(|cause| VaultError::from_io_err(cause, path, IOMode::Write))
-                    })
+            .map_err(|cause| VaultError::from_io_err(cause, path, IOMode::Write))?;
+        serde_yaml::to_writer(&file, self)
+            .map_err(|cause| VaultError::Serialization {
+                cause,
+                path: path.to_owned(),
             })
+            .and_then(|_| {
+                writeln!(file).map_err(|cause| VaultError::from_io_err(cause, path, IOMode::Write))
+            })
+    }
+}
+
+pub fn init(gpg_key_ids: Vec<String>, vault_path: &str) -> Result<String, Error> {
+    let mut gpg_ctx = GpgContext::from_protocol(Protocol::OpenPgp)?;
+    let keys: Vec<_> = gpg_ctx
+        .find_secret_keys(&gpg_key_ids)?
+        .filter_map(Result::ok)
+        .collect();
+    match keys.len() {
+        0 => Err(err_msg(
+            "No existing GPG key found for which you have a secret key. Please create one and try again.",
+        )),
+        x => {
+            if x > 1 && gpg_key_ids.len() == 0 {
+                Err(format_err!("Found {} viable keys for key-ids {:?}, which is ambiguous. Please specify one with the --gpg-key-id argument.", x, gpg_key_ids))
+            } else {
+                let vault = Vault {
+                    at: at_default(),
+                    gpg_keys: Some(String::from(".gpg-keys")),
+                    recipients: String::from(".recipients"),
+                };
+                vault.to_file(vault_path)?;
+                Ok(format!("vault initialized at '{}'", vault_path))
+            }
+        }
     }
 }
 
 /// A universal handler which delegates all functionality based on the provided Context
 /// The latter is usually provided by the user interface.
 pub fn do_it(ctx: Context) -> Result<String, Error> {
-    use types::VaultCommand;
     match ctx.command {
-        VaultCommand::Init { gpg_key_ids } => {
-            let mut gpg_ctx = GpgContext::from_protocol(Protocol::OpenPgp)?;
-            let keys: Vec<_> = gpg_ctx
-                .find_secret_keys(&gpg_key_ids)?
-                .filter_map(Result::ok)
-                .collect();
-            match keys.len() {
-                0 => Err(err_msg(
-                    "No existing GPG key found for which you have a secret key. Please create one and try again.",
-                )),
-                x => {
-                    if x > 1 && gpg_key_ids.len() == 0 {
-                        Err(format_err!("Found {} viable keys for key-ids {:?}, which is ambiguous. Please specify one with the --gpg-key-id argument.", x, gpg_key_ids))
-                    } else {
-                        let vault = Vault {
-                            at: at_default(),
-                            gpg_keys: Some(String::from(".gpg-keys")),
-                            recipients: String::from(".recipients"),
-                        };
-                        vault.to_file(&ctx.vault_path)?;
-                        Ok(format!("vault initialized at '{}'", ctx.vault_path))
-                    }
-                }
-            }
-        }
+        VaultCommand::Init { gpg_key_ids } => init(gpg_key_ids, &ctx.vault_path),
         VaultCommand::List => {
             Vault::from_file(&ctx.vault_path)?;
             Ok(String::new())
