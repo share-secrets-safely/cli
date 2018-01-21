22760067f48b29dc4256e285f2a218d94700d792
Sebastian Thiel <byronimo@gmail.com>
2018-01-21 12:33:33 +0100

    Make 'recipients add' a little less massive.

--- lib/vault/src/recipients/add.rs
+++ lib/vault/src/recipients/add.rs
@@ -1,6 +1,6 @@
 extern crate sheesy_types;
 
-use failure::{Fail, err_msg, Error, ResultExt};
+use failure::{err_msg, Error, ResultExt};
 use std::fs::File;
 use std::io::Write;
 use util::{ResetCWD, strip_ext};
@@ -10,28 +10,6 @@ use mktemp::Temp;
 use error::EncryptionError;
 use util::{write_at, fingerprint_of, new_context, UserIdFingerprint, KeyDisplay, KeylistDisplay, export_key};
 use sheesy_types::SigningMode;
-
-fn valid_fingerprint(id: &str) -> Result<&str, Error> {
-    if id.len() < 8 || id.len() > 40 {
-        return Err(format_err!(
-            "Fingerprint '{}' must be between 8 and 40 characters long.",
-            id
-        ));
-    }
-    let has_only_fingerprint_chars = id.chars().all(|c| match c {
-        'a'...'f' | 'A'...'F' | '0'...'9' => true,
-        _ => false,
-    });
-
-    if has_only_fingerprint_chars {
-        Ok(id)
-    } else {
-        Err(format_err!(
-            "Fingerprint '{}' must only contain characters a-f, A-F and 0-9.",
-            id
-        ))
-    }
-}
 
 impl Vault {
     pub fn add_recipients(
@@ -46,62 +24,12 @@ impl Vault {
             let gpg_keys_dir = self.gpg_keys_dir().context(
                 "Adding unverified recipients requires you to use a vault that has the `gpg-keys` directory configured",
             )?;
-            let imported_gpg_keys_ids = gpg_key_ids
-                .iter()
-                .map(|s| {
-                    valid_fingerprint(&s)
-                        .and_then(|fpr| self.read_fingerprint_file(&fpr, &gpg_keys_dir))
-                        .and_then(|(fpr_path, kb)| {
-                            let res = gpg_ctx.import(kb).map_err(|e| {
-                                e.context(format!(
-                                    "Could not import key to gpg key database from content of file at '{}'",
-                                    fpr_path.display()
-                                )).into()
-                            });
-                            writeln!(
-                                output,
-                                "Imported recipient key at path '{}'",
-                                fpr_path.display()
-                            ).ok();
-                            res
-                        })
-                })
-                .fold(Ok(Vec::new()), |r, k| match k {
-                    Ok(imports) => {
-                        r.map(|mut v| {
-                            v.extend(imports.imports().filter_map(|i| {
-                                i.fingerprint().map(ToOwned::to_owned).ok()
-                            }));
-                            v
-                        })
-                    }
-                    Err(e) => {
-                        match r {
-                            Ok(_) => Err(e),
-                            r @ Err(_) => r.map_err(|f| format_err!("{}\n{}", e, f)),
-                        }
-                    }
-                })?;
-            if imported_gpg_keys_ids.len() < gpg_key_ids.len() {
-                panic!(
-                    "You should come and take a look at this! It should not be possible \
-                        to successfully import less keys than specified."
-                )
-            }
-
-            {
-                let mut extra_keys = imported_gpg_keys_ids.clone();
-                extra_keys.retain(|k| !gpg_key_ids.iter().any(|ok| k.ends_with(ok)));
-                if !extra_keys.is_empty() {
-                    return Err(format_err!(
-                        "One of the imported key-files contained more than one recipient.\n\
-                This might mean somebody trying to sneak in their key. The offending fingerprints are listed below\n\
-                {}",
-                        extra_keys.join("\n")
-                    ));
-                }
-            }
-
+            let imported_gpg_keys_ids = self.import_keys(
+                &mut gpg_ctx,
+                &gpg_keys_dir,
+                gpg_key_ids,
+                output,
+            )?;
             let signing_key = self.find_signing_key(&mut gpg_ctx, signing_key_id)
                 .context(
                     "Did not manage to find suitable signing key \
--- lib/vault/src/recipients/util.rs
+++ lib/vault/src/recipients/util.rs
@@ -1,6 +1,6 @@
-use failure::{err_msg, Error, ResultExt};
+use failure::{Fail, err_msg, Error, ResultExt};
 use std::fs::File;
-use std::io::Read;
+use std::io::{Write, Read};
 use util::ResetCWD;
 use base::Vault;
 use std::path::{PathBuf, Path};
@@ -9,7 +9,94 @@ use util::{fingerprint_of, UserIdFingerprint};
 use gpgme::{self, Key};
 use itertools::Itertools;
 
+fn valid_fingerprint(id: &str) -> Result<&str, Error> {
+    if id.len() < 8 || id.len() > 40 {
+        return Err(format_err!(
+            "Fingerprint '{}' must be between 8 and 40 characters long.",
+            id
+        ));
+    }
+    let has_only_fingerprint_chars = id.chars().all(|c| match c {
+        'a'...'f' | 'A'...'F' | '0'...'9' => true,
+        _ => false,
+    });
+
+    if has_only_fingerprint_chars {
+        Ok(id)
+    } else {
+        Err(format_err!(
+            "Fingerprint '{}' must only contain characters a-f, A-F and 0-9.",
+            id
+        ))
+    }
+}
+
 impl Vault {
+    pub fn import_keys(
+        &self,
+        gpg_ctx: &mut gpgme::Context,
+        gpg_keys_dir: &Path,
+        gpg_key_ids: &[String],
+        output: &mut Write,
+    ) -> Result<Vec<String>, Error> {
+        let imported_gpg_keys_fprs = gpg_key_ids
+            .iter()
+            .map(|s| {
+                valid_fingerprint(&s)
+                    .and_then(|fpr| self.read_fingerprint_file(&fpr, &gpg_keys_dir))
+                    .and_then(|(fpr_path, kb)| {
+                        let res = gpg_ctx.import(kb).map_err(|e| {
+                            e.context(format!(
+                                "Could not import key to gpg key database from content of file at '{}'",
+                                fpr_path.display()
+                            )).into()
+                        });
+                        writeln!(
+                            output,
+                            "Imported recipient key at path '{}'",
+                            fpr_path.display()
+                        ).ok();
+                        res
+                    })
+            })
+            .fold(Ok(Vec::new()), |r, k| match k {
+                Ok(imports) => {
+                    r.map(|mut v| {
+                        v.extend(imports.imports().filter_map(|i| {
+                            i.fingerprint().map(ToOwned::to_owned).ok()
+                        }));
+                        v
+                    })
+                }
+                Err(e) => {
+                    match r {
+                        Ok(_) => Err(e),
+                        r @ Err(_) => r.map_err(|f| format_err!("{}\n{}", e, f)),
+                    }
+                }
+            })?;
+        if imported_gpg_keys_fprs.len() < gpg_key_ids.len() {
+            panic!(
+                "You should come and take a look at this! It should not be possible \
+                        to successfully import less keys than specified."
+            )
+        }
+
+        {
+            let mut extra_keys = imported_gpg_keys_fprs.clone();
+            extra_keys.retain(|k| !gpg_key_ids.iter().any(|ok| k.ends_with(ok)));
+            if !extra_keys.is_empty() {
+                return Err(format_err!(
+                    "One of the imported key-files contained more than one recipient.\n\
+                This might mean somebody trying to sneak in their key. The offending fingerprints are listed below\n\
+                {}",
+                    extra_keys.join("\n")
+                ));
+            }
+        }
+        Ok(imported_gpg_keys_fprs)
+    }
+
     pub fn find_signing_key(&self, ctx: &mut gpgme::Context, signing_key_id: Option<&str>) -> Result<Key, Error> {
         let recipients_fprs = self.recipients_list().context(
             "A recipients list is needed assure the signing key is in the recipients list.",
