32a353090432226da359886119a8c2e633cf4ba9
Sebastian Thiel <sthiel@thoughtworks.com>
2018-03-11 16:45:55 +0100

    The first bunch of 'merge' subcommand tests
    [skip CI]

--- tests/stateless-journey-test.sh
+++ tests/stateless-journey-test.sh
@@ -12,11 +12,124 @@ source "$root/gpg-helpers.sh"
 WITH_FAILURE=1
 SUCCESSFULLY=0
 
-title "'substitute' subcommand"
 fixture="$root/journeys/fixtures"
+template="$fixture/merge"
+snapshot="$fixture/snapshots/merge/stateless"
+
+title "'merge' subcommand"
+(with "yaml from stdin"
+  INPUT="answer: 42"
+  (with "no file to merge"
+    (with "no output specification"
+      it "outputs json" && {
+        echo "$INPUT" | \
+        WITH_SNAPSHOT="$snapshot/yaml-stdin-to-stdout-output-format-unspecified" \
+        expect_run $SUCCESSFULLY "$exe" merge
+      }
+    )
+    (with "explicit yaml output"
+      it "outputs yaml" && {
+        echo "$INPUT" | \
+        WITH_SNAPSHOT="$snapshot/yaml-stdin-to-stdout-as-yaml" \
+        expect_run $SUCCESSFULLY "$exe" merge -o yaml
+      }
+    )
+  )
+  (with "the default merge mode"
+    (with "multiple yaml documents from stdin and from file"
+      it "merges all of them" && {
+          WITH_SNAPSHOT="$snapshot/multi-document-yaml-stdin-to-stdout" \
+          expect_run $SUCCESSFULLY "$exe" merge "$template/multi-docs-1.yml" < "$template/multi-docs-2.yml"
+      }
+    )
+    (with "a single simple yaml file"
+      (with "a single similar value"
+        it "succeeds, as there is no conflict" && {
+          echo "$INPUT" | \
+          WITH_SNAPSHOT="$snapshot/fail-yaml-stdin-with-yaml-file-same-scalar-value-to-stdout" \
+          expect_run $WITH_FAILURE "$exe" merge "$template/good-answer.yml"
+        }
+      )
+    )
+    (with "a two similar complex yaml files"
+      it "succeeds, as there is no conflict" && {
+        WITH_SNAPSHOT="$snapshot/fail-no-stdin-with-two-similar-complex-yaml-files-to-stdout" \
+        expect_run $WITH_FAILURE "$exe" merge "$template/complex.yml" "$template/complex.yml"
+      }
+    )
+    (with "a single complex yaml file"
+      (with "a conflicting nested scalar value from stdin"
+        it "succeeds, as there is no conflict" && {
+          echo "a_nested_map.another_nested_map.hello: world" | \
+          WITH_SNAPSHOT="$snapshot/yaml-conflicting-nested-scalar-stdin-complex-yaml-file-to-stdout" \
+          expect_run $WITH_FAILURE "$exe" merge "$template/complex.yml"
+        }
+      )
+      (with "a conflicting nested scalar value from stdin"
+        it "succeeds, as there is no conflict" && {
+          echo "a_nested_map.another_nested_map.hello: world" | \
+          WITH_SNAPSHOT="$snapshot/yaml-conflicting-nested-scalar-stdin-complex-yaml-file-to-stdout" \
+          expect_run $WITH_FAILURE "$exe" merge "$template/complex.yml"
+        }
+      )
+    )
+    (with "a single conflicting file"
+      (with "conflicting scalar value"
+        (with "yaml format"
+          it "fails as it refuses to overwrite keys" && {
+            echo "$INPUT" | \
+            WITH_SNAPSHOT="$snapshot/fail-yaml-stdin-with-yaml-file-conflicting-scalar-to-stdout" \
+            expect_run $WITH_FAILURE "$exe" merge "$template/wrong-answer.yml"
+          }
+        )
+        (with "json format"
+          it "fails as it merges safely by default" && {
+            echo "$INPUT" | \
+            WITH_SNAPSHOT="$snapshot/fail-yaml-stdin-with-yaml-file-conflicting-scalar-to-stdout" \
+            expect_run $WITH_FAILURE "$exe" merge "$template/wrong-answer.json"
+          }
+        )
+      )
+    )
+  )
+)
+
+title "'merge' subcommand errors"
+(with "invalid output format"
+  it "fails" && {
+    WITH_SNAPSHOT="$snapshot/fail-invalid-output-format" \
+    expect_run $WITH_FAILURE "$exe" merge --output foobar
+  }
+)
+(with "no data from stdin"
+  (with "no given file"
+    it "fails" && {
+      WITH_SNAPSHOT="$snapshot/fail-no-data-from-stdin-and-no-file-specified" \
+      expect_run $WITH_FAILURE "$exe" merge
+    }
+  )
+)
+(with "data invalid data from stdin"
+  (with "yaml format"
+    it "fails" && {
+      echo "a: -" | \
+      WITH_SNAPSHOT="$snapshot/fail-invalid-yaml-stdin" \
+      expect_run $WITH_FAILURE "$exe" merge
+    }
+  )
+  (with "json format"
+    it "fails" && {
+      echo "invalid-json" | \
+      WITH_SNAPSHOT="$snapshot/fail-invalid-json-stdin" \
+      expect_run $WITH_FAILURE "$exe" merge
+    }
+  )
+)
+
 template="$fixture/substitute"
 snapshot="$fixture/snapshots/substitute/stateless"
 
+title "'substitute' subcommand"
 (with "stdin for data"
   (with "input as json"
     (with "single template from a file (absolute path)"
