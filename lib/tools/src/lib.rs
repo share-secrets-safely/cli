d96e9a760fd3ecdda29e0dd433bd96af7ba59abd
Sebastian Thiel <sthiel@thoughtworks.com>
2018-04-05 18:37:40 +0200

    Add feature flags to tools
    [skip CI]

--- lib/tools/src/lib.rs
+++ lib/tools/src/lib.rs
@@ -1,15 +1,26 @@
-extern crate atty;
 #[macro_use]
 extern crate failure;
-extern crate base64;
-extern crate glob;
-extern crate handlebars;
-extern crate liquid;
-extern crate serde;
 extern crate serde_json as json;
 extern crate serde_yaml as yaml;
+
+#[cfg(feature = "substitute")]
+extern crate atty;
+#[cfg(feature = "substitute")]
+extern crate base64;
+#[cfg(feature = "process")]
+extern crate glob;
+#[cfg(feature = "substitute")]
+extern crate handlebars;
+#[cfg(feature = "substitute")]
+extern crate liquid;
+#[cfg(feature = "process")]
+extern crate serde;
+#[cfg(feature = "process")]
 extern crate treediff;
+#[cfg(any(feature = "substitute", feature = "process"))]
 extern crate yaml_rust;
 
+#[cfg(feature = "substitute")]
 pub mod substitute;
+#[cfg(feature = "process")]
 pub mod merge;
