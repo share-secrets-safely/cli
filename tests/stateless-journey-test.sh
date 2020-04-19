312719a3a8b9d7c3a6329b63348341bd703f027c
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-04-19 13:34:32 +0800

    try again…seems somehow 'null' is injected to stdin somehow.

--- tests/stateless-journey-test.sh
+++ tests/stateless-journey-test.sh
@@ -14,9 +14,6 @@ SUCCESSFULLY=0
 
 fixture="$root/journeys/fixtures"
 
-# shellcheck source=./tests/included-stateless-substitute.sh
-source "$root/included-stateless-substitute.sh"
-
 # shellcheck source=./tests/included-stateless-extract.sh
 source "$root/included-stateless-extract.sh"
 
@@ -25,4 +22,7 @@ source "$root/included-stateless-vault.sh"
 
 # shellcheck source=./tests/included-stateless-merge.sh
 source "$root/included-stateless-merge.sh"
+
+# shellcheck source=./tests/included-stateless-substitute.sh
+source "$root/included-stateless-substitute.sh"
 
