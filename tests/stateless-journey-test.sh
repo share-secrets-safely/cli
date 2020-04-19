5ffe5a9b6c4aaec892b8e70180f65f6000c45109
Sebastian Thiel <sebastian.thiel@icloud.com>
2020-04-19 13:56:02 +0800

    Try again, maybe the problem is to run make first

--- tests/stateless-journey-test.sh
+++ tests/stateless-journey-test.sh
@@ -14,11 +14,11 @@ SUCCESSFULLY=0
 
 fixture="$root/journeys/fixtures"
 
-# shellcheck source=./tests/included-stateless-vault.sh
-source "$root/included-stateless-vault.sh"
-
 # shellcheck source=./tests/included-stateless-merge.sh
 source "$root/included-stateless-merge.sh"
+
+# shellcheck source=./tests/included-stateless-vault.sh
+source "$root/included-stateless-vault.sh"
 
 # shellcheck source=./tests/included-stateless-substitute.sh
 source "$root/included-stateless-substitute.sh"
